b16f4000652a27b611ad748924d014ded12f0ca5
pompeucosta <pompeugcosta@gmail.com>
2023-10-13 19:03:45 +0100

    server time request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return(
+        <>
+            <ServerTime />
+        </>
+    )
+  
 }
+
+function ServerTime() {
+    const SERVER_API = "https://localhost:7217/api"
+    const [time,setTime] = useState('')
+
+    const fetch_server_time = async () => {
+        try{
+            const response = await fetch(`${SERVER_API}/Time/Server`)
+            const data = await response.json()
+            setTime(data.serverTime)
+        }
+        catch{
+            setTime("failed to get server time")
+        }
+    }
+
+    useEffect(() => {
+        fetch_server_time()
+    },[])
+
+    return(
+        <h1>{time}</h1>
+    )
+}
+
+
 
 export default App;

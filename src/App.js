6c750d6246b124597e8e3f4d56e6b9a836fa1a56
pompeucosta <pompeugcosta@gmail.com>
2023-10-20 17:40:11 +0100

    added user homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MainPage from "./pages/MainPage"
 import ServerTime from "./pages/ServerTime";
 import NoPage from "./pages/NoPage"
+import UserHomePage from "./pages/UserHomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainPage />}/>
+                <Route path="/home" element={<UserHomePage />}/>
                 <Route path="serverTime" element={<ServerTime />} />
                 <Route path="*" element={<NoPage />} />
             </Routes>

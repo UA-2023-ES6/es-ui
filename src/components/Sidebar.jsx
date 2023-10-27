b8b4cbcb89f0cb067d6b6e82ca4f3bd9e70d359e
pompeucosta <pompeugcosta@gmail.com>
2023-10-28 00:08:01 +0100

    added the add btn to sidebar items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,11 +21,21 @@ function SidebarEntry({content,baseLink}) {
     return(
         <div className="d-flex flex-column p-3 bg-body-tertiary" style={{width: "280px",flex: "1"}}>
             <ul className="nav nav-pills flex-column mb-auto">
+                <li className="nav-item ps-3">
+                    <div className="d-flex add-btn-group">
+                        <div style={{flex: "1"}}>
+                            {"test"}
+                        </div>
+                        <div className="add-btn">
+                            {"test2"}
+                        </div>
+                    </div>
+                </li>
                 {
                     Object.entries(content).map(([_name,_content]) => {
                         return(
                             <li className="nav-item ps-3" key={_name}>
-                                <LinkButton name={_name} parentLink={baseLink}/>
+                                <LinkElement name={_name} parentLink={baseLink} includeAddBtn={true}/>
                                 <div className="d-flex flex-column">
                                     <ul className="nav nav-pills flex-column mb-auto">
                                         {
@@ -69,9 +79,12 @@ function CollapsableGroup({content= null,parent}) {
 function Group({name,content,parent}) {
     return(
         <>
-            <div className="d-flex align-items-center">
-                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target={"#" + name.replace(/\s/g, "") + "-collapse"} aria-expanded="true"/>
-                <LinkButton name={name} parentLink={parent}/>
+            <div className="d-flex add-btn-group align-items-center">
+                <div className="d-flex align-items-center" style={{flex: "1"}}>
+                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target={"#" + name.replace(/\s/g, "") + "-collapse"} aria-expanded="true"/>
+                    <LinkElement name={name} parentLink={parent}/>
+                </div>
+                <button className="btn add-btn"/>
             </div>
             <div className="collapse show" style={{paddingLeft: "2rem"}} id={name.replace(/\s+/g, "") + "-collapse"}>
                 <ul className="btn-toggle-nav list-unstyled">
@@ -87,22 +100,38 @@ function Group({name,content,parent}) {
 function ListElement({text,parent}) {
     return(
         <li className="ps-4">
-            <LinkButton name={text} parentLink={parent} />
+            <LinkElement name={text} parentLink={parent} includeAddBtn={true}/>
         </li>
     )
 }
 
-function LinkButton({name,parentLink}) {
+function LinkElement({name,parentLink,includeAddBtn=false}) {
     var link = parentLink + name.replace(/\s+/g, "")
     var active = link == _activeLink
+    const x = <LinkButton link={link} name={name} active={active} />
     return(
         <>
-            <Link to={link}>
-                <button className={"btn border-0 rounded p-2 " + (active ? "btn-primary" : "")}>
-                    {name}
-                </button>
-            </Link>
+            <div className="d-flex add-btn-group align-items-center">
+                {
+                    includeAddBtn ? <><div style={{flex: "1"}}>
+                                {x}
+                            </div>
+                            <button className="btn add-btn"/>
+                            </>
+                            : <>{x}</>
+                }
+            </div>
         </>
+    )
+}
+
+function LinkButton({link,name,active}) {
+    return(
+        <Link to={link}>
+            <button className={"btn border-0 rounded p-2 " + (active ? "btn-primary" : "")}>
+                {name}
+            </button>
+        </Link>
     )
 }
 

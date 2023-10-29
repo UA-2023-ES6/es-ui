797c916f06f9f0621a24e60ce13a3089f7c0db5f
pompeucosta <pompeugcosta@gmail.com>
2023-10-29 17:07:12 +0000

    changed sidebar implementation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,128 +1,41 @@
 import { Link } from "react-router-dom"
 import "../styles/sidebar.css"
 
-var _activeLink = ""
-//exemplo de activeLink: Instituicao/test/subturma1
-
-const Sidebar = ({content,activeLink="",baseLink="",onClick=(name,parentLink) => {}}) => {
-    _activeLink = activeLink
-    var x = ""
-    if(content != null) {
-        x = <SidebarEntry content={content} baseLink={baseLink} onClick={onClick}/>
-    }
-    return(
-        <>
-            {x}
-        </>
-    )
-}
-
-function SidebarEntry({content,baseLink,onClick=(name,parentLink) => {}}) {
+const Sidebar = ({children}) => {
     return(
         <div className="d-flex flex-column p-3 bg-body-tertiary" style={{width: "280px",flex: "1"}}>
             <ul className="nav nav-pills flex-column mb-auto">
-                {
-                    Object.entries(content).map(([_name,_content]) => {
-                        return(
-                            <li className="nav-item ps-3" key={_name}>
-                                <LinkElement name={_name} parentLink={baseLink} includeAddBtn={true} onClick={onClick}/>
-                                <div className="d-flex flex-column">
-                                    <ul className="nav nav-pills flex-column mb-auto">
-                                        {
-                                            <CollapsableGroup content={_content} parent={_name.replace(/\s/g) + "/"} onClick={onClick}/>
-                                        }
-                                    </ul>
-                                </div>
-                            </li>
-                        )
-                    })
-                }
+                {children}
             </ul>
         </div>
     )
 }
 
-function CollapsableGroup({content= null,parent,onClick=(name,parentLink) => {}}) {
-    var x = ""
-
-    if(content != null) {
-        if(content.constructor == Object) {
-            x = Object.entries(content).map(([_name,_content]) => {
-                return <Group name={_name} content={_content} parent={parent} key={_name} onClick={onClick}/>
-            })
-        }
-        else {
-            x = content.map((_name) => {
-                return <ListElement text={_name} parent={parent} key={_name} onClick={onClick}/>
-            })
-        }
-    }
-
-
+const SidebarGroupHeader = ({children,header,headerKey}) => {
     return(
         <>
-            {x}
-        </>
-    )
-}
-
-function Group({name,content,parent,onClick=(name,parentLink) => {}}) {
-    return(
-        <>
-            <div className="d-flex add-btn-group align-items-center">
-                <div className="d-flex align-items-center" style={{flex: "1"}}>
-                    <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target={"#" + name.replace(/\s/g, "") + "-collapse"} aria-expanded="true"/>
-                    <LinkElement name={name} parentLink={parent}/>
-                </div>
-                <button className="btn add-btn" onClick={() => {onClick(name,parent)}}/>
-            </div>
-            <div className="collapse show" style={{paddingLeft: "2rem"}} id={name.replace(/\s+/g, "") + "-collapse"}>
-                <ul className="btn-toggle-nav list-unstyled">
-                    {
-                        <CollapsableGroup content={content} parent={parent + name.replace(/\s+/g, "") + "/"} onClick={onClick}/>
-                    }
+        <li className="nav-item ps-3" >
+            <div className="d-flex flex-column">
+                <ul className="nav nav-pills flex-column mb-auto">
+                    {header}
+                    <div className="collapse show" style={{paddingLeft: "2rem"}} id={headerKey.replace(/\s+/g, "") + "-collapse"}>
+                        <ul className="btn-toggle-nav list-unstyled">
+                            {children}
+                        </ul>
+                    </div>
                 </ul>
             </div>
-        </>
+        </li>
+    </>
     )
 }
 
-function ListElement({text,parent,onClick=(name,parentLink) => {}}) {
+const SidebarElement = ({children}) => {
     return(
         <li className="ps-4">
-            <LinkElement name={text} parentLink={parent} includeAddBtn={true} onClick={onClick}/>
+            {children}
         </li>
     )
 }
 
-function LinkElement({name,parentLink,includeAddBtn=false,onClick=(name,parentLink) => {}}) {
-    var link = parentLink + name.replace(/\s+/g, "")
-    var active = link == _activeLink
-    const x = <LinkButton link={link} name={name} active={active} />
-    return(
-        <>
-            <div className="d-flex add-btn-group align-items-center">
-                {
-                    includeAddBtn ? <><div style={{flex: "1"}}>
-                                {x}
-                            </div>
-                            <button className="btn add-btn" onClick={() => {onClick(name,parentLink)}}/>
-                            </>
-                            : <>{x}</>
-                }
-            </div>
-        </>
-    )
-}
-
-function LinkButton({link,name,active}) {
-    return(
-        <Link to={link}>
-            <button className={"btn border-0 rounded p-2 " + (active ? "btn-primary" : "")}>
-                {name}
-            </button>
-        </Link>
-    )
-}
-
-export default Sidebar
+export {Sidebar,SidebarGroupHeader,SidebarElement}
--- src/pages/Dummy.jsx
+++ src/pages/Dummy.jsx
@@ -1,21 +1,106 @@
 import { Link, useParams } from "react-router-dom"
-import Sidebar from "../components/Sidebar"
+import {Sidebar,SidebarGroupHeader,SidebarElement} from "../components/Sidebar"
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-function test(name,parent) {
-    alert("name:" + name + "; parent:" + parent)
+const test = {
+    id: 1,
+    name: "UA",
+    groups:
+    [
+        {
+            id: 1,
+            name:"turma1",
+            groups:
+            [
+                {
+                id: 1,
+                name: "grupo1",
+                usersIds: [
+                    {
+                    id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    name: "user1",
+                    email: "user1@mail.com"
+                    }
+                ]
+                },
+                {
+                    id: 2,
+                    name: "grupo2",
+                    usersIds: [
+                    {
+                        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        name: "user1",
+                        email: "user1@mail.com"
+                    }
+                    ]
+                },
+                {
+                    id: 3,
+                    name: "grupo3",
+                    usersIds: [
+                    {
+                        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                        name: "user1",
+                        email: "user1@mail.com"
+                    }
+                    ]
+                }
+            ] 
+        }
+    ]
 }
 
 const Dummy = () => {
+    const SERVER_API = "https://localhost:7217/api"
     const {"*": currentPath} = useParams()
     
+    const [groupName,setGroupName] = useState("")
+    const [path,setPath] = useState("")
+
     const [show,setShow] = useState(false)
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
+    const handleChange = (e) => setGroupName(e.target.value)
+    const handleCreate = () => {
+        console.log(groupName)
+        const data = {
+            name: groupName,
+            classId: 1
+        }
 
-    console.log(currentPath)
+        const t = JSON.stringify(data)
+        console.log(t)
+
+        fetch(`${SERVER_API}/Group`,{
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: t
+        })
+        handleClose()
+    }
+
+
+
+    // const t = async () => {
+    //     try{
+    //         const response = await fetch(`${SERVER_API}/Group?Take=10&Skip=0`)
+    //         const data = await response.json()
+    //         console.log(data.data)
+    //     }
+    //     catch {
+    //         console.log("error")
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     t()
+    // },[])
+
     if(currentPath === "") {
         return(
             <>
@@ -32,30 +117,53 @@ const Dummy = () => {
             </>
         )
     }
-    const instituicao = {
-        "Instituicao": {
-                "turma1": ["grupo1","grupo2","grupo3"],
-                "test": {
-                    "sub turma1": ["grupo1","grupo2","grupo3"],
-                    "sub turma2": ["grupo1","grupo2","grupo3"]
-                },
-                "turma2": ["grupo1","grupo2","grupo3"],
-                "turma3": ["grupo1","grupo2","grupo3"],
-                "turma4": ["grupo1","grupo2","grupo3"],
-            }
-        }
+
     return(
         <>
             <div className="d-flex flex-column" style={{height: "100%"}}>
-                <CreateGroupModal show={show} handleClose={handleClose}/>
-                <Sidebar content={instituicao} activeLink={currentPath} onClick={handleShow}/>
-                {/* alternative a onClick seria inline: (name,parent) => { conteudo da funcao } */}
+                <CreateGroupModal show={show} handleClose={handleClose} onNameChange={handleChange} onCreate={handleCreate}/>
+                <MySidebar content={test} onAddClick={handleShow} onElementClick={(path) => {setPath(path)}} activeLink={path} basePath={""}/>
             </div>
         </>
     )
 }
 
-function CreateGroupModal({show,handleClose}) {
+function MySidebar({content,onAddClick,onElementClick,activeLink,basePath}) {
+    return(
+        <Sidebar>
+            <SidebarContent content={content} onAddClick={onAddClick} onElementClick={onElementClick} activeLink={activeLink} parentPath={basePath}/>
+        </Sidebar>
+    )
+}
+
+function SidebarContent({content,onAddClick,parentPath,onElementClick,activeLink}) {
+    var innerContent = ""
+    const path = parentPath + "/" + content.name
+    if(content.constructor === Object && "groups" in content) {
+        innerContent = <>
+            <SidebarGroupHeader header={<Header title={content.name} onAddClick={onAddClick} activeLink={activeLink} onElementClick={onElementClick} path={path}/>} headerKey={content.name}>
+                {
+                    content.groups.map((group) => {return <SidebarContent key={group.name} content={group} onAddClick={onAddClick} parentPath={path} onElementClick={onElementClick} activeLink={activeLink}/>})
+                }
+            </SidebarGroupHeader>
+        </>
+    }
+    else {
+        innerContent = <>
+            <SidebarElement>
+                <LinkElement name={content.name} activeLink={activeLink} onAddClick={onAddClick} path={path} onElementClick={onElementClick}/>
+            </SidebarElement>
+        </>
+    }
+
+    return(
+        <>
+            {innerContent}
+        </>
+    )
+}
+
+function CreateGroupModal({show,handleClose,onNameChange,onCreate}) {
     return(
         <>
             <Modal show={show} onHide={handleClose}>
@@ -65,8 +173,8 @@ function CreateGroupModal({show,handleClose}) {
                 <Modal.Body>
                 <form>
                     <div className="mb-3">
-                        <label for="formGroupName" className="form-label">Group name</label>
-                        <input type="text" className="form-control" id="formGroupName"/>
+                        <label className="form-label">Group name</label>
+                        <input type="text" className="form-control" id="formGroupName" onChange={onNameChange} />
                     </div>
                 </form>
                 </Modal.Body>
@@ -74,12 +182,51 @@ function CreateGroupModal({show,handleClose}) {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={onCreate}>
                         Create
                     </Button>
                 </Modal.Footer>
             </Modal>
         </>
+    )
+}
+
+//sidebar helper functions
+function Header({title,onAddClick,activeLink,onElementClick,path}) {
+    return(
+        <div className="d-flex add-btn-group align-items-center">
+            <div className="d-flex align-items-center" style={{flex: "1"}}>
+                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target={"#" + title.replace(/\s/g, "") + "-collapse"} aria-expanded="true"/>
+                <LinkButton name={title} activeLink={activeLink} onElementClick={onElementClick} path={path}/>
+            </div>
+            <AddBtn onClick={onAddClick} />
+        </div>
+    )
+}
+
+function LinkElement({activeLink,name,onAddClick,path,onElementClick}) {
+    return(
+        <div className="d-flex add-btn-group align-items-center">
+            <div style={{flex: "1"}}>
+                <LinkButton name={name} activeLink={activeLink} path={path} onElementClick={onElementClick}/>
+            </div>
+            <AddBtn onClick={onAddClick} />
+        </div>
+    )
+}
+
+function LinkButton({name,activeLink,path,onElementClick}) {
+    const active = activeLink == path
+    return(
+        <button className={"btn border-0 rounded p-2 " + (active ? "btn-primary" : "")} onClick={() => {onElementClick(path)}}>
+            {name}
+        </button>
+    )
+}
+
+function AddBtn({onClick}) {
+    return(
+        <button className="btn add-btn" onClick={onClick}/>
     )
 }
 

667a27d4f9b95014fc5c3cb5a4487afd2f3adc01
pompeucosta <pompeugcosta@gmail.com>
2023-10-29 17:39:54 +0000

    implemented getGroups and post group

--- src/pages/Dummy.jsx
+++ src/pages/Dummy.jsx
@@ -14,50 +14,54 @@ const test = {
             name:"turma1",
             groups:
             [
-                {
-                id: 1,
-                name: "grupo1",
-                usersIds: [
-                    {
-                    id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                    name: "user1",
-                    email: "user1@mail.com"
-                    }
-                ]
-                },
-                {
-                    id: 2,
-                    name: "grupo2",
-                    usersIds: [
-                    {
-                        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                        name: "user1",
-                        email: "user1@mail.com"
-                    }
-                    ]
-                },
-                {
-                    id: 3,
-                    name: "grupo3",
-                    usersIds: [
-                    {
-                        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                        name: "user1",
-                        email: "user1@mail.com"
-                    }
-                    ]
-                }
+                // {
+                // id: 1,
+                // name: "grupo1",
+                // usersIds: [
+                //     {
+                //     id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                //     name: "user1",
+                //     email: "user1@mail.com"
+                //     }
+                // ]
+                // },
+                // {
+                //     id: 2,
+                //     name: "grupo2",
+                //     usersIds: [
+                //     {
+                //         id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                //         name: "user1",
+                //         email: "user1@mail.com"
+                //     }
+                //     ]
+                // },
+                // {
+                //     id: 3,
+                //     name: "grupo3",
+                //     usersIds: [
+                //     {
+                //         id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                //         name: "user1",
+                //         email: "user1@mail.com"
+                //     }
+                //     ]
+                // }
             ] 
         }
     ]
 }
 
+const SERVER_API = "https://localhost:7217/api"
+
 const Dummy = () => {
-    const SERVER_API = "https://localhost:7217/api"
     const {"*": currentPath} = useParams()
-    
     const [groupName,setGroupName] = useState("")
-    const [path,setPath] = useState("")
+    const [path,setPath] = useState("/" + test.name)
+    const [sidebarContent,setSidebarContent] = useState([])
+    test.groups[0].groups = sidebarContent
+
+    const [updateFlag,setUpdateFlag] = useState(false)
 
     const [show,setShow] = useState(false)
     const handleClose = () => setShow(false)
@@ -82,24 +86,15 @@ const Dummy = () => {
             body: t
         })
         handleClose()
+        setUpdateFlag(!updateFlag)
     }
 
 
-
-    // const t = async () => {
-    //     try{
-    //         const response = await fetch(`${SERVER_API}/Group?Take=10&Skip=0`)
-    //         const data = await response.json()
-    //         console.log(data.data)
-    //     }
-    //     catch {
-    //         console.log("error")
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     t()
-    // },[])
+    useEffect(() => {
+        getGroups().then((data) => {
+            setSidebarContent(data)
+        })
+    },[updateFlag])
 
     if(currentPath === "") {
         return(
@@ -128,6 +123,18 @@ const Dummy = () => {
     )
 }
 
+async function getGroups() {
+    try{
+        const response = await fetch(`${SERVER_API}/Group?Take=100&Skip=0`)
+        const data = await response.json()
+        return data.data
+    }
+    catch {
+        console.log("error")
+        return []
+    }
+}
+
 function MySidebar({content,onAddClick,onElementClick,activeLink,basePath}) {
     return(
         <Sidebar>
@@ -136,14 +143,14 @@ function MySidebar({content,onAddClick,onElementClick,activeLink,basePath}) {
     )
 }
 
-function SidebarContent({content,onAddClick,parentPath,onElementClick,activeLink}) {
+function SidebarContent({content,onAddClick,parentPath,onElementClick,activeLink,innerLevel=1}) {
     var innerContent = ""
     const path = parentPath + "/" + content.name
     if(content.constructor === Object && "groups" in content) {
         innerContent = <>
-            <SidebarGroupHeader header={<Header title={content.name} onAddClick={onAddClick} activeLink={activeLink} onElementClick={onElementClick} path={path}/>} headerKey={content.name}>
+            <SidebarGroupHeader header={<Header title={content.name} onAddClick={onAddClick} activeLink={activeLink} onElementClick={onElementClick} path={path} innerLevel={innerLevel}/>} headerKey={content.name}>
                 {
-                    content.groups.map((group) => {return <SidebarContent key={group.name} content={group} onAddClick={onAddClick} parentPath={path} onElementClick={onElementClick} activeLink={activeLink}/>})
+                    content.groups.map((group) => {return <SidebarContent key={group.name} content={group} onAddClick={onAddClick} parentPath={path} onElementClick={onElementClick} innerLevel={innerLevel+1} activeLink={activeLink}/>})
                 }
             </SidebarGroupHeader>
         </>
@@ -151,7 +158,7 @@ function SidebarContent({content,onAddClick,parentPath,onElementClick,activeLink
     else {
         innerContent = <>
             <SidebarElement>
-                <LinkElement name={content.name} activeLink={activeLink} onAddClick={onAddClick} path={path} onElementClick={onElementClick}/>
+                <LinkElement name={content.name} activeLink={activeLink} onAddClick={onAddClick} path={path} onElementClick={onElementClick} innerLevel={innerLevel}/>
             </SidebarElement>
         </>
     }
@@ -192,31 +199,31 @@ function CreateGroupModal({show,handleClose,onNameChange,onCreate}) {
 }
 
 //sidebar helper functions
-function Header({title,onAddClick,activeLink,onElementClick,path}) {
+function Header({title,onAddClick,activeLink,onElementClick,path,innerLevel}) {
     return(
         <div className="d-flex add-btn-group align-items-center">
             <div className="d-flex align-items-center" style={{flex: "1"}}>
                 <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target={"#" + title.replace(/\s/g, "") + "-collapse"} aria-expanded="true"/>
                 <LinkButton name={title} activeLink={activeLink} onElementClick={onElementClick} path={path}/>
             </div>
-            <AddBtn onClick={onAddClick} />
+            {innerLevel === 2 ? <AddBtn onClick={onAddClick} /> : ""}
         </div>
     )
 }
 
-function LinkElement({activeLink,name,onAddClick,path,onElementClick}) {
+function LinkElement({activeLink,name,onAddClick,path,onElementClick,innerLevel}) {
     return(
         <div className="d-flex add-btn-group align-items-center">
             <div style={{flex: "1"}}>
                 <LinkButton name={name} activeLink={activeLink} path={path} onElementClick={onElementClick}/>
             </div>
-            <AddBtn onClick={onAddClick} />
+            {innerLevel === 2 ? <AddBtn onClick={onAddClick} /> : ""}
         </div>
     )
 }
 
 function LinkButton({name,activeLink,path,onElementClick}) {
-    const active = activeLink == path
+    const active = activeLink === path
     return(
         <button className={"btn border-0 rounded p-2 " + (active ? "btn-primary" : "")} onClick={() => {onElementClick(path)}}>
             {name}
